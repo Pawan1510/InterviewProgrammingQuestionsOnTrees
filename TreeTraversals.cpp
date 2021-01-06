f6e8f3c5e4b97b32ec5f028bc8476afc3a2b3e06
PAWAN KUMAR <47324615+Pawan1510@users.noreply.github.com>
2021-01-06 17:39:03 +0530

    VerticalOrder Traversal Function Added

--- TreeTraversals.cpp
+++ TreeTraversals.cpp
@@ -55,6 +55,27 @@ void levelOrder( Node *root )
 
     }
 }
+void getVerticalOrder( Node *root, int hd, map< int, vector<int> > &m )
+{
+    if( root == NULL )  return;
+    m[hd].push_back( root->data );
+
+    getVerticalOrder( root->left, hd - 1, m );
+
+    getVerticalOrder( root->right, hd + 1, m );
+}
+void printVerticalOrder(  Node *root )
+{
+    map< int, vector<int> > m;
+    int hd = 0;
+    getVerticalOrder( root, hd, m );
+    for( auto it = m.begin(); it != m.end(); it++ ) {
+        for( int i=0; i<it->second.size(); i++ ) {
+            cout<<it->second[i]<<" ";
+        }
+        cout<<endl;
+    }
+}
 int main()
 {
     Node *root = new Node(1);
@@ -75,9 +96,10 @@ int main()
                     \    \
                      8    9
     */
-    cout<<"Inorder Traversal: ";    inOrder(root);      cout<<endl;
-    cout<<"Preorder Traversal: ";   preOrder(root);     cout<<endl;
-    cout<<"PostOrder Traversal: ";  postOrder(root);    cout<<endl;
-    cout<<"LevelOrder Traversal: "; levelOrder(root);   cout<<endl;
+    cout<<"Inorder Traversal: ";        inOrder(root);              cout<<endl;
+    cout<<"Preorder Traversal: ";       preOrder(root);             cout<<endl;
+    cout<<"PostOrder Traversal: ";      postOrder(root);            cout<<endl;
+    cout<<"LevelOrder Traversal: ";     levelOrder(root);           cout<<endl;
+    cout<<"VerticalOrder Traversal: ";  cout<<endl; printVerticalOrder(root);   cout<<endl;
     return 0;
 }
